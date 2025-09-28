9b6ac45c7f1c3aa91e2161e7d7af969118a1f6cf
Phuong Tran <phuong0968804740@gmail.com>
2025-09-28 19:39:14 +0700

    feat: add ESP32 firmware for water sensors

--- monitoring/mixins.py
+++ monitoring/mixins.py
@@ -5,13 +5,12 @@ class RoleBasedPermission(permissions.BasePermission):
     Permission class để kiểm tra vai trò người dùng trong API Views
     """
     def has_permission(self, request, view):
-        # Lấy danh sách roles được phép từ view
         allowed_roles = getattr(view, 'allowed_roles', [])
         
         if not request.user.is_authenticated:
             return False
             
-        if not allowed_roles:  # Nếu không định nghĩa roles, cho phép tất cả
+        if not allowed_roles:
             return True
             
         return request.user.role in allowed_roles
